358ef4e26ac89d5238dbb25f907aa5a21d6e0ef1
Matheus Henrique Maia Ramos <matheus.henrimr@gmail.com>
2024-03-21 14:20:45 -0300

    fix. create-forms.dto
    createFormsDto classes now don't rely on exact same names of the body they've received from frontend.

--- projetoNest/projeto-nest/src/forms/dto/create-forms.dto.ts
+++ projetoNest/projeto-nest/src/forms/dto/create-forms.dto.ts
@@ -1,30 +1,29 @@
 // DTO FOR CREATION OF FORMS
 
-//2ª FORMA --> PADRÃO QUE USAREMOS
+//1ª FORMA --> PADRÃO QUE USAREMOS
 export class CreateFormsDto {
-    constructor(
-        public conteudoTexto: String,
-        public inteiro: Number,
-        public booleano: Boolean,
-        public opcaoSelect: String,
-        public opcaoRadio: String
-    ){}
+    public conteudoTexto: String;
+    public inteiro: Number;
+    public booleano: Boolean;
+    public opcaoSelect: String;
+    public opcaoRadio: String;
+
+    constructor(text: String, inteiro, Number, booleano: Boolean, opcaoSelect: String, opcaoRadio: String){
+        this.conteudoTexto = text;
+        this.inteiro = inteiro;
+        this.booleano = booleano;
+        this.opcaoSelect = opcaoSelect;
+        this.opcaoRadio = opcaoRadio;
+    }
 }
 
-
-//1ª FORMA
+//2ª FORMA
 // export class CreateFormsDto {
-//     public conteudoTexto: String;
-//     public inteiro: Number;
-//     public booleano: Boolean;
-//     public opcaoSelect: String;
-//     public opcaoRadio: String;
-
-//     constructor(text: String, inteiro, Number, booleano: Boolean, opcaoSelect: String, opcaoRadio: String){
-//         this.text = text;
-//         this.inteiro = inteiro;
-//         this.booleano = booleano;
-//         this.opcaoSelect = opcaoSelect;
-//         this.opcaoRadio = opcaoRadio;
-//     }
+//     constructor(
+//         public conteudoTexto: String,
+//         public inteiro: Number,
+//         public booleano: Boolean,
+//         public opcaoSelect: String,
+//         public opcaoRadio: String
+//     ){}
 // }
