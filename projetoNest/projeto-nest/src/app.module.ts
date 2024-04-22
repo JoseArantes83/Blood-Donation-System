298a284d8064065fa1ea026894398c5fa484d60a
Matheus Henrique Maia Ramos <matheus.henrimr@gmail.com>
2024-04-22 13:45:34 -0300

    feat. following YT tutorial Nestjs PostgreSQL CRUD REST API example with Typeorm
    https://www.youtube.com/watch?v=pI9C7NH4K5c

--- projetoNest/projeto-nest/src/app.module.ts
+++ projetoNest/projeto-nest/src/app.module.ts
@@ -3,9 +3,42 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DoadorModule } from './doador/doador.module';
 import { DoacaoModule } from './doacao/doacao.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { Doacao } from './doacao/entities/doacao.entity';
+// import { Doador } from './doador/entities/doador.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-  imports: [DoadorModule, DoacaoModule],
+  imports: [DoadorModule, DoacaoModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        // entities: [Doacao, Doador],
+        entities: [join(process.cwd(), 'src/**/*.entity.js')],
+        //Nao usar synchronize em projetos reais 
+        synchronize: true,
+      })
+    })
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'admin',
+    //   password: '123456',
+    //   database: 'postgres',
+    //   entities: [Doacao, Doador],
+    //   synchronize: true,
+    // })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- projetoNest/projeto-nest/src/doacao/entities/doacao.entity.ts
+++ projetoNest/projeto-nest/src/doacao/entities/doacao.entity.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { LocalDate, LocalTime } from '@js-joda/core';
 
 @Entity()
 export class Doacao {
